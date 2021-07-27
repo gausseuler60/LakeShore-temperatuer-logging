46a44751b27aaa9400dd7462ab015e44b25f910e
Ivan Nazhestkin <61228847+gausseuler60@users.noreply.github.com>
2021-07-27 17:59:28 +0300

    integration with Overseer bot by @vdrhtc

--- ARS_4K_monitor.py
+++ ARS_4K_monitor.py
@@ -1,18 +1,20 @@
 import threading
 import os
 from os import path
-import visa
 import threading
 import time
 import wx
 import wx.adv
-from Drivers.LakeShore335 import LakeShore335
 
+from Drivers.LakeShore335 import LakeShore335
+from ARS_4K_remote import ARS_4K_slave
 
 TRAY_TOOLTIP = 'ARS 4K cryostat logging tool'
 TRAY_ICON = 'Monitor.ico'
 
 log_file_name = 'Temperature.log'
+auth_file_name = 'overseer_auth.dat'
+temp_buffer_size = 10
 
 
 def ensure_logging_directories():
@@ -33,28 +35,44 @@ def check_day_change():
         current_logging_file = path.join(ensure_logging_directories(), log_file_name)
 
 
-def perform_logging_record(device: LakeShore335):
-    device.temp_channel = 'A'
-    temp_A = device.GetTemperature()
-    device.temp_channel = 'B'
-    temp_B = device.GetTemperature()
-    time_to_write = time.strftime('%H-%M-%S')
+def add_temperatures_to_lists(temp_A, temp_B):
+    last_temps_A.append(temp_A)
+    last_temps_B.append(temp_B)
+    if len(last_temps_A) > temp_buffer_size:
+        last_temps_A.pop(0)
+        last_temps_B.pop(0)
 
+
+def perform_logging_record(temp_A, temp_B):
+    time_to_write = time.strftime('%H-%M-%S')
     with open(current_logging_file, 'a') as f:
         f.write(f'{time_to_write} {temp_A} {temp_B}\n')
 
 
+def scan_temperatures(device: LakeShore335):
+    device.temp_channel = 'A'
+    temp_A = device.GetTemperature()
+    device.temp_channel = 'B'
+    temp_B = device.GetTemperature()
+    add_temperatures_to_lists(temp_A, temp_B)
+    return temp_A, temp_B
+
 def logging_thread_proc():
     ls = LakeShore335(device_num=12, control_channel='A', heater_channel=1, mode='passive')
+    overseer_authorize(ls)
+
+    temp_A, temp_B = scan_temperatures(ls)
+    perform_logging_record(temp_A, temp_B)
+
     time_start = time.time()
-    perform_logging_record(ls)
     while not event_exit.is_set():
         time_now = time.time()
+        temp_A, temp_B = scan_temperatures(ls)
         if time_now - time_start >= 30:
             check_day_change()
-            perform_logging_record(ls)
+            perform_logging_record(temp_A, temp_B)
             time_start = time_now
-        time.sleep(1)
+        time.sleep(5)
 
 
 def create_menu_item(menu, label, func):
@@ -63,6 +81,28 @@ def create_menu_item(menu, label, func):
     menu.Append(item)
     return item
 
+
+def get_bot_login_password():
+    try:
+        with open(auth_file_name, 'r') as f:
+            for i, line in enumerate(f):
+                if i == 0:
+                    login = line.strip()
+                elif i == 1:
+                    password = line.strip()
+        return login, password
+    except Exception as e:
+        print('Cannot authorize in Overseer bot, invalid credentials file')
+        return "", ""
+
+
+def overseer_authorize(lakeshore):
+    login, password = get_bot_login_password()
+    if len(login) == 0:
+        return
+    bot = ARS_4K_slave(login, password, 'triangle.enricherclub.com', 23137,
+                       last_temps_A, last_temps_B, temp_buffer_size)
+    bot.launch()
 
 class TaskBarIcon(wx.adv.TaskBarIcon):
     def __init__(self):
@@ -91,6 +131,9 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
         event_exit.set()
         wx.CallAfter(self.Destroy)
 
+
+last_temps_A = []
+last_temps_B = []
 
 current_logging_file = path.join(ensure_logging_directories(), log_file_name)
 event_exit = threading.Event()
---
+++ ARS_4K_remote.py
@@ -0,0 +1,53 @@
+import numpy as np
+from copy import copy
+
+from slave import Slave
+from Drivers.LakeShore335 import LakeShore335
+
+LAKESHORE_MODEL_370 = 0
+LAKESHORE_MODEL_335 = 1
+
+
+class ARS_4K_slave(Slave):
+    def __init__(self, nickname, password, server_address, server_port, temp_list_A, temp_list_B, temp_buffer_size):
+        self._temps_A = temp_list_A
+        self._temps_B = temp_list_B
+        self._buffer_size = temp_buffer_size
+        self._xaxis = np.linspace(1, self._buffer_size, self._buffer_size)
+        # self._last_event_check_time = datetime.now()
+        super().__init__(nickname, password, server_address, server_port)
+
+    # def generate_alert_messages(self):
+    # TODO: maybe add messages about some events
+    #     return []
+
+    def generate_info_message(self):
+        if len(self._temps_A) == 0:
+            return "Please wait, loading..."
+
+        temp_A = self._temps_A[-1]
+        temp_B = self._temps_B[-1]
+
+        # Determine warming or cooling status
+        if len(self._temps_A) >= 5:
+            frozen_temps = copy(self._temps_A)
+            n_frozen_temps = len(frozen_temps)
+            rng = np.linspace(0, n_frozen_temps, n_frozen_temps)
+            temps_approx_k = np.polyfit(rng, frozen_temps, deg=1)[0]
+            if temps_approx_k > 0:
+                status = 'Warming'
+            else:
+                status = 'Cooling'
+            if abs(self._temps_A[-1] - self._temps_A[0]) < 0.2:
+                status = 'Approx. stable'
+        else:
+            status = 'Gathering statistics...'
+
+        message = f'Temperatures:\nChannel A: {temp_A:.3f} K\nChannel B: {temp_B:.3f}K'
+
+        final = status + '\n' + message
+
+        return final
+
+
+
